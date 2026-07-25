2ecc7ff9dba1be5335f0d10bf5d9bbc3b2119488
pletony <142932918+pletony525@users.noreply.github.com>
2026-07-25 14:14:05 -0500

    Add: linked-list-cycle-detection - submission-7

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-7.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        even, odd = head, head
+        while even and even.next:
+            even = even.next.next
+            odd = odd.next
+            if even == odd:
+                return True
+            
+            
+        return False
+        
